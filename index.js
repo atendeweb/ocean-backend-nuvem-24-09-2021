e439c5ff74ff5292cd8d2ed089f2897e44cd75e3
Paulo Rocha <acesso@yahoo.com.br>
2021-09-24 12:01:40 -0300

    usando url do professor

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 (async () => {
-    const url = "mongodb+srv://admin:mother@cluster0.q0lzs.mongodb.net/";
+    const url = "mongodb+srv://admin:hd2rV5duoPrrIi3t@cluster0.jup2c.mongodb.net/";
     const dbName = "ocean_bancodados_22_09_2021";
 
     const client = await MongoClient.connect(url);
